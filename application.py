02641b0bb26f6d709403f04a30d04ba11df5c6a3
Kaj <madalitsomuva@gmail.com>
2020-05-15 14:50:07 +0200

    using sqlachemy methods

--- application.py
+++ application.py
@@ -1,17 +1,15 @@
-import os
 from flask import Flask, render_template, request
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from models import *
 
 app = Flask(__name__)
 
-engine = create_engine("postgres+psycopg2://dba:kajani1993@localhost/lecture2")
-db = scoped_session(sessionmaker(bind=engine))
-engine.connect()
+app.config["SQLALCHEMY_DATABASE_URI"]= "postgres+psycopg2://admin:kajani1993@localhost/lecture5"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False
+db.init_app(app)
 
 @app.route("/")
 def index():
-    flights = db.execute("SELECT * FROM flights").fetchall()
+    flights = Flight.query.order_by(Flight.origin).all()
     return render_template("index.html", flights=flights)
 
 @app.route("/book", methods=["POST"])
@@ -21,21 +19,22 @@ def book():
         flightid = int(request.form.get("fid"))
     except ValueError:
         return render_template("error.html", message="Invalid Flight Number.")
-    if db.execute("SELECT * FROM flights WHERE id =:id", {"id": flightid}).rowcount==0:
-        return render_template("error.html", message="No such flight with that ID.")
-    db.execute("INSERT INTO passengers (name, flightid ) VALUES (:name, :flightid)",
-            {"name" : name, "flightid":flightid})
-    db.commit()
+
+    flight = Flight.query.get(flightid)
+    if flight is None:
+        return render_template("error.html", message="Invalid ID.")
+
+    flight.add_passenger(name)
     return render_template("success.html")
 @app.route("/flights")
 def flights():
-    allflights = db.execute("SELECT * FROM flights").fetchall()
+    allflights = Flight.query.order_by(Flight.origin).all()
     return render_template("flights.html", flights=allflights)
 
 @app.route("/flights/<int:flightid>")
 def flight(flightid):
-    flight = db.execute("SELECT * FROM flights WHERE id = :id", {"id": flightid}).fetchone()
+    flight = Flight.query.get(flightid)
     if flight is None:
         return render_template("error.html", message="No such flight.")
-    passengers = db.execute("SELECT name FROM passengers WHERE flightid =:id", {"id": flightid}).fetchall()
+    passengers = flight.passengers
     return render_template("flight.html", flight=flight, passengers=passengers)
